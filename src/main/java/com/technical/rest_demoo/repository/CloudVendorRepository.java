8e40d3fc7bc41819b0dc4d47c22c880b164f031a
Ananya Guin <ananyaguin96@gmail.com>
2026-05-11 20:45:12 +0530

    Added JPA repository for database operations

--- src/main/java/com/technical/rest_demoo/repository/CloudVendorRepository.java
+++ src/main/java/com/technical/rest_demoo/repository/CloudVendorRepository.java
@@ -1,8 +1,6 @@
 package com.technical.rest_demoo.repository;
-
 import com.technical.rest_demoo.model.CloudVendor;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 public interface CloudVendorRepository extends JpaRepository <CloudVendor,String>{
 
 
