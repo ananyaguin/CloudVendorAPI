d0737a8bf13c524824d3000359c1c96e51ba55d6
Ananya Guin <ananyaguin96@gmail.com>
2026-05-13 03:25:37 +0530

    Added repository

--- src/main/java/com/technical/rest_demoo/repository/CustomerRepository.java
+++ src/main/java/com/technical/rest_demoo/repository/CustomerRepository.java
@@ -5,4 +5,7 @@ import com.technical.rest_demoo.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, String> {
+
+
+
 }
